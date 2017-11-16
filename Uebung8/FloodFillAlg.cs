18c18e8ae465d1ee88024ff89cc8f89061ee7179
Tobias Bollinger <tobias.bollinger@students.fhnw.ch>
2017-11-16 19:37:27 +0100

    Added Task 8

---
+++ Uebung8/FloodFillAlg.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Uebung8
+{
+    public static class FloodFillAlg
+    {
+        private static Image<Rgb, byte> _newImg;
+        private static byte[,,] _data;
+        private static byte[,,] _newData;
+
+        private static Color[] _colors =
+        {
+            Colors.Blue, Colors.Red, Colors.Green, Colors.Yellow, Colors.Orange, Colors.Violet, Colors.Pink,
+            Colors.YellowGreen, Colors.Aqua
+        };
+
+        public static Image<Rgb, byte> Run(Image<Gray, byte> img)
+        {
+            byte m = 2;
+
+            _newImg = new Image<Rgb, byte>(img.Width, img.Height);
+            _data = img.Data;
+            _newData = _newImg.Data;
+
+            for (int u = 0; u < img.Width; u++)
+            {
+                for (int v = 0; v < img.Height; v++)
+                {
+                    if (_data[v, u, 0] == 1)
+                    {
+                        FloodFill(u, v, m);
+                        checked
+                        {
+                            m++;
+                        }
+                    }
+                }
+            }
+
+            return _newImg;
+        }
+
+        private static void FloodFill(int u, int v, byte label)
+        {
+            var (w, h) = (_newImg.Width, _newImg.Height);
+            var queue = new Queue<(int u, int v)>();
+            queue.Enqueue((u,v));
+
+            while (queue.Count > 0)
+            {
+                var (x, y) = queue.Dequeue();
+
+                if (x > 0 && x < w & y > 0 && y < h && _data[y, x, 0] == 1)
+                {
+                    _data[y, x, 0] = label;
+                    var i = label % _colors.Length;
+
+                    _newData[y, x, 0] = _colors[i].R;
+                    _newData[y, x, 1] = _colors[i].G;
+                    _newData[y, x, 2] = _colors[i].B;
+
+                    queue.Enqueue((x + 1, y));
+                    queue.Enqueue((x, y + 1));
+                    queue.Enqueue((x, y - 1));
+                    queue.Enqueue((x - 1, y));
+                }
+            }
+        }
+    }
+}
---
+++ Uebung8/MainWindowViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Uebung8.Commands;
+
+namespace Uebung8
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private BitmapImage _newImage;
+        private BitmapImage _oldImage;
+        private string _imgPath = @"coins.png";
+
+
+        public ICommand OpenFileCommand { get; set; }
+        public ICommand FloodFillCommand { get; set; }
+
+        public BitmapImage NewImage
+        {
+            get => _newImage;
+            set
+            {
+                if (Equals(value, _newImage)) return;
+                _newImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BitmapImage OldImage
+        {
+            get => _oldImage;
+            set
+            {
+                if (Equals(value, _oldImage)) return;
+                _oldImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            FloodFillCommand = new SimpleCommand(FloodFill);
+        }
+
+        public void FloodFill()
+        {
+            var img = new Image<Gray, byte>((Bitmap)Image.FromFile(_imgPath));
+            var binbaryImage = img.ThresholdBinary(new Gray(90), new Gray(1));
+            CvInvoke.MedianBlur(binbaryImage, binbaryImage, 3);
+            
+            var labelImage = FloodFillAlg.Run(binbaryImage);
+
+            NewImage = ToBitmapImage(labelImage.Bitmap);
+            OldImage = ToBitmapImage(img.Bitmap);
+        }
+
+        private void OpenFile()
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog()
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*PNG;*TIF)|*.BMP;*.JPG;*.GIF;*PNG;*TIF"
+            };
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    _imgPath = fileDialog.FileName;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    return;
+            }
+
+            OldImage = ToBitmapImage((Bitmap)Image.FromFile(_imgPath));
+        }
+
+        public BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
